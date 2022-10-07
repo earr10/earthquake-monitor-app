a0ce010e8e5abfb3404a339d4e43c16904b5701c
earr10 <erick_rr1@outlook.com>
2022-10-06 19:51:49 -0500

    EmptyView when Rv has no content

--- app/src/main/java/mx/com/earr/earthquakemonitorapp/MainActivity.kt
+++ app/src/main/java/mx/com/earr/earthquakemonitorapp/MainActivity.kt
@@ -2,6 +2,7 @@ package mx.com.earr.earthquakemonitorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import mx.com.earr.earthquakemonitorapp.databinding.ActivityMainBinding
 
@@ -24,5 +25,12 @@ class MainActivity : AppCompatActivity() {
         val adapter = EarthquakeAdapter()
         binding.eqRecycler.adapter = adapter
         adapter.submitList(eqList)
+
+        if (eqList.isEmpty()) {
+            binding.eqEmptyView.visibility = View.VISIBLE
+        } else {
+            binding.eqEmptyView.visibility = View.GONE
+        }
+
     }
 }
